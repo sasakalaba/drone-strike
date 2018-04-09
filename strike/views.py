a8462e082014b54e9bc5c4b1515158707b39ebe2
sasakalaba <sasa.kalaba@yahoo.com>
2018-04-09 11:46:56 +0200

    Added index view.

--- strike/views.py
+++ strike/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Strike
 
 
 def index(request):
     template = loader.get_template('index.html')
-    context = {}
+    context = {'strikes': Strike.objects.all()}
 
     return HttpResponse(template.render(context, request))
