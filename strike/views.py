41cccc6c5a235f2ee8a5bed43d615954be059788
sasakalaba <sasa.kalaba@yahoo.com>
2018-04-10 01:00:42 +0200

    Frontend second draft.

--- strike/views.py
+++ strike/views.py
@@ -5,6 +5,7 @@ from .models import Location
 
 def index(request):
     template = loader.get_template('index.html')
+    # template = loader.get_template('test_map.html')
 
     # locations = Location.objects.all()
     locations = Location.objects.exclude(lat__exact='', lon__exact='')
